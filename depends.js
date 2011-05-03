e09e85c7062715a135cddec3dfa80eb81eadbe35
Ed Finkler <coj@funkatron.com>
2011-05-03 10:32:51 -0400

    load up new stuff

--- depends.js
+++ depends.js
@@ -4,14 +4,29 @@ enyo.depends(
 	"vendors/underscore-min.js",
 	"vendors/underscore.string.min.js",
 	"vendors/spazcore-standard.js",
+
+	// config
+	"source/javascript/config/auth_config.js",
+	"source/javascript/config/default_preferences.js",
+
+	// constants we use
+	"source/javascript/resources/consts.js",
+
+	// helpers
+
+	// models
+
 	//core
 	"source/javascript/Spaz.js",
+	
 	//views
 	"source/javascript/Sidebar.js",
 	"source/javascript/Container.js", //container for columns
 		"source/javascript/Column.js",
+	
 	//popouts
 	"source/javascript/EntryView.js",
+	
 	//popups
 	"source/javascript/ComposePopup.js",
 	"source/javascript/SettingsPopup.js",
