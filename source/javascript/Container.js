17b1aedc059881a9e0132fe0f5740dd71412a6d2
Ed Finkler <coj@funkatron.com>
2011-05-06 13:13:58 -0400

    minor adjustments to column creation to make it a bit more robust.

--- source/javascript/Container.js
+++ source/javascript/Container.js
@@ -17,12 +17,19 @@ enyo.kind({
 	createColumns: function() {
 		// we should load this from prefs
 		
+		var firstAccount = App.Users.getAll()[0];
+
+		if (!firstAccount || !firstAccount.id) {
+			alert('no accounts! you should add one');
+			return;
+		}
+
 		var colData = [
-			{type: "home", display: "Home", accounts: [App.Users.getAll()[0].id]},
-			{type: "mentions", display: "Mentions", accounts: [App.Users.getAll()[0].id]},
+			{type: "home", display: "Home", accounts: [firstAccount.id]},
+			{type: "mentions", display: "Mentions", accounts: [firstAccount.id]},
 			// {type: "dms", display: "Messages", accounts: [App.Users.getAll()[0].id]},
-			{type: "search", query: 'webos', display: "Search&nbsp;'webos'", accounts: [App.Users.getAll()[0].id]},
-			{type: "search", query: 'spaz', display: "Search&nbsp;'spaz'", accounts: [App.Users.getAll()[0].id]},
+			{type: "search", query: 'webos', display: "Search&nbsp;'webos'", accounts: [firstAccount.id]},
+			{type: "search", query: 'spaz', display: "Search&nbsp;'spaz'", accounts: [firstAccount.id]},
 		]
 
 		var cols = [];
