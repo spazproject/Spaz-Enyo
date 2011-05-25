5647e9d2cea321761f3ca66b3c06ce62e7311681
Will Honey <tibfib1@gmail.com>
2011-05-25 12:08:42 -0500

    some updates to SettingsPopup.js

--- source/javascript/SettingsPopup.js
+++ source/javascript/SettingsPopup.js
@@ -4,6 +4,8 @@ enyo.kind({
 	scrim: true,
 	modal: true,
 	width: "400px",
+	height: "600px",
+	layoutKind: "VFlexLayout",
 	events: {
 		onClose: ""
 	},
@@ -13,6 +15,63 @@ enyo.kind({
 			{kind: "Spacer"},
 			{kind: "ToolButton", icon: "source/images/icon-close.png", style: "position: relative; bottom: 7px;", onclick: "doClose"}
 		]},	
+		{kind: "Scroller", flex: 1, components: [
+			{kind: "Group", caption: "Columns", components: [
+				{kind: "Item", layoutKind: "HFlexLayout", components: [
+					{content: "Default Width"},
+					{kind: "Spacer"},
+					{kind: "ListSelector", items: [
+						
+					
+					]}
+				]},
+
+			]},
+			{kind: "Group", caption: "Entries", components: [
+				{kind: "Item", content: "Text Size"}, //list selector // 10 - 20px
+				{kind: "Item", layoutKind: "HFlexLayout", components: [
+					{content: "Embedded Image Preview"},
+					{kind: "Spacer"},
+					{kind: "CheckBox", onChange: "checkboxClicked"}
+				]},
+
+			]},
+			{kind: "Group", caption: "Compose", components: [
+				{kind: "Item", layoutKind: "HFlexLayout", components: [
+					{content: "Enter Posts"},
+					{kind: "Spacer"},
+					{kind: "CheckBox", onChange: "checkboxClicked"}
+				]},
+				{kind: "Item", content: "URL Shortening"}, //list selector
+			]},
+			{kind: "Group", caption: "Refresh", components: [
+				{kind: "Item", content: "Interval"}, //list selector
+				//{kind: "Item", content: "Interval for Searches"},
+			]},
+			{kind: "Group", caption: "Notify", components: [
+				{kind: "Item", layoutKind: "HFlexLayout", components: [
+					{content: "New Entries"},
+					{kind: "Spacer"},
+					{kind: "CheckBox", onChange: "checkboxClicked"}
+				]},
+				{kind: "Item", layoutKind: "HFlexLayout", components: [
+					{content: "Mentions"},
+					{kind: "Spacer"},
+					{kind: "CheckBox", onChange: "checkboxClicked"}
+				]},
+				{kind: "Item", layoutKind: "HFlexLayout", components: [
+					{content: "Private Messages"},
+					{kind: "Spacer"},
+					{kind: "CheckBox", onChange: "checkboxClicked"}
+				]},
+				{kind: "Item", layoutKind: "HFlexLayout", components: [
+					{content: "Search Results"},
+					{kind: "Spacer"},
+					{kind: "CheckBox", onChange: "checkboxClicked"}
+				]}
+			]},
+		]}
+		
 	],
 	create: function(){
 		this.inherited(arguments);
