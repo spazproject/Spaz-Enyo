70e2ea110acec129992bbfa9d723b1ef5134d4f4
Will Honey <tibfib1@gmail.com>
2011-06-01 15:44:44 -0500

    Added favoriting to EntryClickPopup

--- source/javascript/EntryClickPopup.js
+++ source/javascript/EntryClickPopup.js
@@ -46,6 +46,44 @@ enyo.kind({
 				AppUtils.showBanner($L('Not yet implemented'));
 				this.clearEntry();
 				break;
+			case "Favorite":
+			case "Unfavorite":
+				var that = this;
+				var account = App.Users.get(this.entry.account_id);
+				var auth = new SpazAuth(account.type);
+				auth.load(account.auth);
+					
+				var twit =new SpazTwit();
+				twit.setBaseURLByService(account.type);
+				twit.setSource(App.Prefs.get('twitter-source'));
+				twit.setCredentials(auth);
+					
+				if (that.entry.is_favorite) {
+					console.log('UNFAVORITING %j', that.entry);
+					twit.unfavorite(
+						that.entry.service_id,
+						function(data) {
+							that.entry.is_favorite = false;
+							AppUtils.showBanner($L('Removed favorite'));
+						},
+						function(xhr, msg, exc) {
+							AppUtils.showBanner($L('Error removing favorite'));
+						}
+					);
+				} else {
+					console.log('FAVORITING %j', that.entry);
+					twit.favorite(
+						that.entry.service_id,
+						function(data) {
+							that.entry.is_favorite = true;
+							AppUtils.showBanner($L('Added favorite'));								
+						},
+						function(xhr, msg, exc) {
+							AppUtils.showBanner($L('Error adding favorite'));
+						}
+					);
+				}
+				break;
 			default: 
 				console.error(this.$.popup.getItems()[inIndex] + " has no handler");
 				break;
@@ -56,6 +94,20 @@ enyo.kind({
 			this.validateComponents();
 		}
 		this.entry = inEntry;
+		var popupItems = [
+			"Details",
+			"Reply",
+			"Repost",
+			"Edit & Repost",
+			"Share"
+		];
+		if(this.entry.is_favorite){
+			popupItems.push("Unfavorite");
+		} else if(!this.entry.is_private_message){
+			popupItems.push("Favorite");
+		}
+		this.$.popup.setItems(popupItems);
+		
 		this.$.popup.openAtEvent(inEvent);
 	},
 	getEntry: function() {
