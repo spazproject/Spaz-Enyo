93a0a7fb5aa7a3e0c8828f26a7976d8216681ecd
Nicholas Guarracino <nick@guarracino.org>
2011-06-07 15:19:45 -0400

    Added share submenu

--- source/javascript/EntryClickPopup.js
+++ source/javascript/EntryClickPopup.js
@@ -7,112 +7,91 @@ enyo.kind({
 		onShare: ""
 	},
 	components: [
-		{name: "popup", kind: "PopupList", onSelect: "itemSelect", items: [
-			"Details",
-			"Reply",
-			"Repost",
-			"Edit & Repost",
-			"Share"
-		]}
+		{name: "menu", kind: "enyo.Menu"}
 	],
 	create: function(){
 		this.inherited(arguments);
 		this.entry = null;
 	},
-	itemSelect: function(inSender, inIndex){
-
-		switch(this.$.popup.getItems()[inIndex]){
-			case "Details":
-				AppUI.viewEntry(this.entry);
-				this.$.popup.close();
-				break;
-			case "Reply":
-				AppUI.reply(this.entry);
-				this.$.popup.close();
-				break;
-			case "Repost":
-				AppUI.repost(this.entry);
-				this.$.popup.close();
-				break;
-			case "Edit & Repost":
-				AppUI.repostManual(this.entry);
-				this.$.popup.close();
-				break;
-			case "Share":
-				AppUtils.showBanner($L('Not yet implemented'));
-				break;
-			case "Favorite":
-			case "Unfavorite":
-				var that = this;
-				var account = App.Users.get(this.entry.account_id);
-				var auth = new SpazAuth(account.type);
-				auth.load(account.auth);
-					
-				var twit =new SpazTwit();
-				twit.setBaseURLByService(account.type);
-				twit.setSource(App.Prefs.get('twitter-source'));
-				twit.setCredentials(auth);
-					
-				if (that.entry.is_favorite) {
-					console.log('UNFAVORITING %j', that.entry);
-					twit.unfavorite(
-						that.entry.service_id,
-						function(data) {
-							that.entry.is_favorite = false;
-							AppUtils.showBanner($L('Removed favorite'));
-							AppUI.rerenderTimelines();
-						},
-						function(xhr, msg, exc) {
-							AppUtils.showBanner($L('Error removing favorite'));
-						}
-					);
-				} else {
-					console.log('FAVORITING %j', that.entry);
-					twit.favorite(
-						that.entry.service_id,
-						function(data) {
-							that.entry.is_favorite = true;
-							AppUtils.showBanner($L('Added favorite'));
-							AppUI.rerenderTimelines();
-							
-						},
-						function(xhr, msg, exc) {
-							AppUtils.showBanner($L('Error adding favorite'));
-						}
-					);
+	detailsClicked: function(inSender) {
+		AppUI.viewEntry(this.entry);
+	},
+	replyClicked: function(inSender) {
+		AppUI.reply(this.entry);
+	},
+	favoriteClicked: function(inSender) {
+		var account = App.Users.get(this.entry.account_id);
+		var auth = new SpazAuth(account.type);
+		auth.load(account.auth);
+			
+		var twit =new SpazTwit();
+		twit.setBaseURLByService(account.type);
+		twit.setSource(App.Prefs.get('twitter-source'));
+		twit.setCredentials(auth);
+			
+		if (this.entry.is_favorite) {
+			console.log('UNFAVORITING %j', this.entry);
+			twit.unfavorite(
+				this.entry.service_id,
+				enyo.bind(this, function(data) {
+					this.entry.is_favorite = false;
+					AppUtils.showBanner($L('Removed favorite'));
+					AppUI.rerenderTimelines();
+				}),
+				function(xhr, msg, exc) {
+					AppUtils.showBanner($L('Error removing favorite'));
 				}
-				break;
-			default: 
-				console.error(this.$.popup.getItems()[inIndex] + " has no handler");
-				break;
-		}	
-		this.clearEntry();
+			);
+		} else {
+			console.log('FAVORITING %j', this.entry);
+			twit.favorite(
+				this.entry.service_id,
+				enyo.bind(this, function(data) {
+					this.entry.is_favorite = true;
+					AppUtils.showBanner($L('Added favorite'));
+					AppUI.rerenderTimelines();
+					
+				}),
+				function(xhr, msg, exc) {
+					AppUtils.showBanner($L('Error adding favorite'));
+				}
+			);
+		}
+	},
+	repostClicked: function(inSender) {
+		AppUI.repost(this.entry);
+	},
+	editRepostClicked: function(inSender) {
+		AppUI.repostManual(this.entry);
 	},
 	showAtEvent: function(inEntry, inEvent){
 		if(this.lazy) {
 			this.validateComponents();
 		}
 		this.entry = inEntry;
-		var popupItems = [
-			"Details",
-			"Reply",
-			"Repost",
-			"Edit & Repost",
-			"Share"
-		];
-		if(this.entry.is_favorite){
-			popupItems.push("Unfavorite");
-		} else if(!this.entry.is_private_message){
-			popupItems.push("Favorite");
-		}
-		this.$.popup.setItems(popupItems);
 		
-		this.$.popup.openAtEvent(inEvent);
-	},
-	getEntry: function() {
-		return this.entry;
-	},
-	clearEntry: function() {
-		this.entry = null;
+		enyo.forEach (this.$.menu.getControls(), function (control) {
+			 control.destroy();
+		});
+		this.$.menu.openAtEvent(inEvent);
+				
+		var components = [
+			{name: "details", owner: this, caption: "Details", onclick: "detailsClicked"},
+			{name: "reply", owner: this, caption: "Reply", onclick: "replyClicked"}
+		];
+		
+		if(this.entry.is_favorite){
+			components.push({name: "unfavorite", owner: this, caption: "Unfavorite", onclick: "favoriteClicked"});
+		} else if(!this.entry.is_private_message){
+			components.push({name: "favorite", owner: this, caption: "Favorite", onclick: "favoriteClicked"});
+		}
+		
+		components.push({name: "shareMenu", owner: this, caption: "Share", components: [
+			{name: "repost", owner: this, caption: "Repost", onclick: "repostClicked"},
+			{name: "editRepost", owner: this, caption: "Edit & Repost", onclick: "editRepostClicked"}
+		]});
+		
+		this.$.menu.createComponents(components);
+		this.$.menu.render();
 	}
 });
