0c9d0590e5b5f2a2dbfaef62f0a0a1493b372da7
Tibfib <Tibfib1@gmail.com>
2011-06-21 13:58:10 -0500

    If at top, don't let you use the mousewheel to scroll up

--- vendors/ekl/List/VirtualList.js
+++ vendors/ekl/List/VirtualList.js
@@ -12,20 +12,25 @@ enyo.kind({
     
     mousewheelHandler: function(inSender, inEvent) {
         if (this.mousewheel) {
-            //Clone event
-            var dragTo = enyo.mixin({}, inEvent);
-            //Apply delta to new event
-            dragTo.pageX = inEvent.pageX + (inEvent.delta.x * this.mousewheelDamp);
-            dragTo.pageY = inEvent.pageY + (inEvent.delta.y * this.mousewheelDamp);
+                //if at top, don't let you scroll up.
+            if(this.$.scroller.pageTop < 0 || inEvent.delta.y < 0){
 
-            //Simulate initiating a drag
-            this.$.scroller.$.scroll.startDrag(inEvent);
-            //Simulate dragging to a point
-            this.$.scroller.$.scroll.drag(dragTo);
-            //Simulate dropping a drag at the same point (prevents flick, lets OS provide accelleration)
-            this.$.scroller.$.scroll.dragDrop(dragTo);
-            //Simulate ending a drag
-            this.$.scroller.$.scroll.dragFinish();
+                //Clone event
+                var dragTo = enyo.mixin({}, inEvent);
+                //Apply delta to new event
+                dragTo.pageX = inEvent.pageX + (inEvent.delta.x * this.mousewheelDamp);
+                dragTo.pageY = inEvent.pageY + (inEvent.delta.y * this.mousewheelDamp);
+
+                //Simulate initiating a drag
+                this.$.scroller.$.scroll.startDrag(inEvent);
+                //Simulate dragging to a point
+                this.$.scroller.$.scroll.drag(dragTo);
+                //Simulate dropping a drag at the same point (prevents flick, lets OS provide accelleration)
+                this.$.scroller.$.scroll.dragDrop(dragTo);
+                //Simulate ending a drag
+                this.$.scroller.$.scroll.dragFinish();     
+            }
+           
         }
     }
     
