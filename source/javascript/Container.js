97eb73a3bf106f22e69aea5b372135fe62e08055
Ed Finkler <coj@funkatron.com>
2011-07-16 19:51:21 -0400

    strip whitespace

--- source/javascript/Container.js
+++ source/javascript/Container.js
@@ -10,10 +10,10 @@ enyo.kind({
 	columnData: [],
 	components: [
 		{kind: "Spaz.Notifier", name:"notifier"},
-		
+
 		{name:"columnsScroller", kind: "SnapScroller", className: "enyo-hflexbox", flex: 1, vertical: false, autoVertical: false, style: "background-color: black; padding: 2px;", components:[
 		]},
-		
+
 		{name: "confirmPopup", kind: "enyo.Popup", scrim : true, components: [
 			{content: enyo._$L("Delete Column?")},
 			{style: "height: 10px;"},
@@ -27,10 +27,10 @@ enyo.kind({
 
 	create: function(){
 		this.inherited(arguments);
-		
+
 		this.loadingColumns = 0;
 		this.loadAndCreateColumns();
-		
+
 		AppUI.addFunction("search", function(inQuery, inAccountId){
 			this.createColumn(inAccountId, "search", inQuery);
 		}, this);
@@ -47,12 +47,12 @@ enyo.kind({
 			this.$.notifier.raiseNotifications();
 		}, this);
 	},
-	
+
 	loadAndCreateColumns: function() {
 		this.columnData = App.Prefs.get('columns') || [];
 		this.createColumns();
 	},
-	
+
 	getDefaultColumns: function(inAccountId) {
 		if(!inAccountId) {
 			var firstAccount = App.Users.getAll()[0];
@@ -60,7 +60,7 @@ enyo.kind({
 				inAccountId = firstAccount.id;
 			}
 		}
-		
+
 		if (!inAccountId) {
 			AppUtils.showBanner(enyo._$L('No accounts! You should add one.'));
 			setTimeout(enyo.bind(this, this.doShowAccountsPopup, 1));
@@ -75,10 +75,10 @@ enyo.kind({
 
 		return default_columns;
 	},
-	
+
 	createColumns: function() {
 		this.$.columnsScroller.destroyControls();
-		
+
 		if(this.columnData.length === 0){
 			this.columnData = this.getDefaultColumns();
 		}
@@ -101,7 +101,7 @@ enyo.kind({
 
 				onToolbarmousehold: "columnMousehold", onToolbarmouserelease: "columnMouserelease",
 				onToolbardragstart: "columnDragStart", onToolbardrag: "columnDrag", onToolbardragfinish: "columnDragFinish"
-			}; 
+			};
 			if(col.info.type === SPAZ_COLUMN_SEARCH){
 				col.kind = "Spaz.SearchColumn";
 			}
@@ -111,7 +111,7 @@ enyo.kind({
 			if(this.columnData[i].entries){
 				col.entries = this.columnData[i].entries;
 			}
-	
+
 			cols.push(
 				{kind: "Control", name: "ColumnSpacer"+ i, width: "0px", ondragover: "spacerDragOver", ondrop: "spacerDrop", ondragout: "spacerDragOut"},
 				col
@@ -121,11 +121,11 @@ enyo.kind({
 		});
 		this.$.columnsScroller.createComponents(cols, {owner: this});
 		this.$.columnsScroller.render();
-		
+
 		this.saveColumnData();
 	},
 	createColumn: function(inAccountId, inColumn, inQuery){
-				
+
 		this.columnData.push({type: inColumn, accounts: [inAccountId], query: inQuery, id: _.uniqueId(new Date().getTime())});
 
 		this.saveColumnEntries();
@@ -135,10 +135,10 @@ enyo.kind({
 
 	},
 	saveColumnData: function(){
-		
+
 		var save_cols = [];
 		var this_col  = {};
-		
+
 		// copy just the data we want (id, type, accounts, query)
 		_.each(this.columnData, function(columnData){
 			this_col = {};
@@ -148,7 +148,7 @@ enyo.kind({
 			this_col.query = columnData.query;
 			save_cols.push(this_col);
 		});
-		
+
 		App.Prefs.set('columns', save_cols);
 
 		this.saveColumnEntries();
@@ -157,20 +157,20 @@ enyo.kind({
 
 	moveColumnLeft: function(inSender){
 		this.saveColumnEntries();
-		
+
 		var del_idx = parseInt(inSender.name.replace('Column', ''), 10);
 		var column = this.columnData.splice(del_idx, 1)[0];
 		this.columnData.splice(del_idx-1, 0, column);
-		
+
 		this.createColumns();
 	},
 	moveColumnRight: function(inSender){
 		this.saveColumnEntries();
-		
+
 		var del_idx = parseInt(inSender.name.replace('Column', ''), 10);
 		var column = this.columnData.splice(del_idx, 1)[0];
 		this.columnData.splice(del_idx+1, 0, column);
-		
+
 		this.createColumns();
 	},
 	deleteColumn: function(inSender) {
@@ -185,7 +185,7 @@ enyo.kind({
 	confirmColumnDeletion: function(inSender) {
 		this.$.confirmPopup.close();
 		if (this.columnToDelete) {
-			
+
 			// find the index to delete and remove it
 			var del_idx = parseInt(this.columnToDelete.name.replace('Column', ''), 10);
 			this.columnData.splice(del_idx, 1);
@@ -217,18 +217,18 @@ enyo.kind({
 		}, this);
 		return columnCount;
 	},
-	
+
 	refreshAll: function() {
 		this.loadingColumns = 0;
 		if(this.columnsFunction("loadNewer") === 0) {
 			this.loadFinished();
 		}
 	},
-	
+
 	loadStarted: function() {
 		this.loadingColumns++;
 	},
-	
+
 	loadFinished: function() {
 		this.loadingColumns--;
 		if (this.loadingColumns <= 0) {
@@ -240,12 +240,12 @@ enyo.kind({
 	search: function(inSender, inQuery){
 		this.createColumn(inSender.info.accounts[0], "search", inQuery);
 	},
-	
+
 	accountAdded: function(inAccountId) {
 		this.columnData = this.columnData.concat(this.getDefaultColumns(inAccountId));
 		this.createColumns();
 	},
-	
+
 	removeColummnsForAccount: function(inAccountId) {
 		var lengthBefore = this.columnData.length;
 		for(var i = lengthBefore - 1; i >= 0; i--) {
@@ -258,11 +258,11 @@ enyo.kind({
 			this.createColumns();
 		}
 	},
-	
+
 	removeEntryById: function (inEntryId) {
 		this.columnsFunction("removeEntryById", inEntryId);
 	},
-	
+
 	saveColumnEntries: function() {
 		enyo.forEach(this.$.columnsScroller.getControls(), enyo.bind(this, function(control) {
 			if(_.includes(control.kind, "Column") && !_.includes(control.kind, "Spacer")){
@@ -284,7 +284,7 @@ enyo.kind({
 			}));
 			inSender.applyStyle("width", "322px");
 			//console.error("drug over", inSender.name);
-		}	
+		}
 	},
 	spacerDragOut: function(inSender, inEvent){
 		//inSender.applyStyle("width", "5px");
@@ -295,7 +295,7 @@ enyo.kind({
 			//console.error("Dropped on spacer", inSender.name);
 
 			this.saveColumnEntries();
-		
+
 			var del_idx = parseInt(this.activeColumn.name.replace('Column', ''), 10);
 			var new_idx = parseInt(inSender.name.replace('ColumnSpacer', ''), 10);
 			if(new_idx > del_idx){
@@ -303,10 +303,10 @@ enyo.kind({
 			}
 			var column = this.columnData.splice(del_idx, 1)[0];
 			this.columnData.splice(new_idx, 0, column);
-			
+
 			this.createColumns();
 			//@TODO: this creates issues with starting a column drag. To replicate: Hold onto a column toolbar, move it slightly, release. It will float in an awkward postion.
-			//this doesn't happen if this.createColumns() is commented out. 
+			//this doesn't happen if this.createColumns() is commented out.
 		}
 	},
 
@@ -320,14 +320,14 @@ enyo.kind({
 		this.activeColumn.addClass("moving");
 		this.activeColumn.applyStyle("height", window.innerHeight - 12 + "px");
 
-		
+
 		this.trackColumn(inEvent);
 	},
 	columnMouserelease: function(inSender, inEvent){
 		this.isHolding = false;
-		//console.error("mouse release called");		
+		//console.error("mouse release called");
 		if(!this.dragColumn){
-			//console.error("mouse released");		
+			//console.error("mouse released");
 
 			this.activeColumn.removeClass("moving");
 			this.activeColumn.applyStyle("height", null);
@@ -339,7 +339,7 @@ enyo.kind({
 				}
 			}));
 
-			this.activeColumn = undefined;	
+			this.activeColumn = undefined;
 		}
 	},
 
@@ -347,7 +347,7 @@ enyo.kind({
 		if (Math.abs(inEvent.dx) < 200) { //make sure the user isn't trying to scroll
 
 			if(this.isHolding){
-				//console.error("drag start");		
+				//console.error("drag start");
 				enyo.forEach(this.$.columnsScroller.getControls(), enyo.bind(this, function(control) {
 					if(_.includes(control.name, "ColumnSpacer")){
 						control.addClass("columnSpacer");
@@ -361,10 +361,10 @@ enyo.kind({
 					}
 				}));
 
-				
+
 				this.dragColumn = true;
 				inEvent.dragInfo = inSender.name;
-					
+
 				this.trackColumn(inEvent);
 				return true;
 			}
@@ -377,13 +377,13 @@ enyo.kind({
 				//console.log(this.$.columnsScroller.getControls().length-2, this.$.columnsScroller.getIndex());
 				this.$.columnsScroller.next();
 			} else if(inEvent.pageX  < 100){
-				this.$.columnsScroller.previous();				
+				this.$.columnsScroller.previous();
 			}
 		}
 	},
 	columnDragFinish: function(inSender, inEvent){
 		if (this.dragColumn) {
-			//console.error("done dragging column");		
+			//console.error("done dragging column");
 
 			this.activeColumn.removeClass("moving");
 			this.activeColumn.applyStyle("height", null);
