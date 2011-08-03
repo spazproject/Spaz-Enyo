2331228a3c9ea7f10552385d8db5443e685bc642
Ed Finkler <coj@funkatron.com>
2011-08-03 17:10:26 -0400

    wrap this.createColumns() in a setTimeout to fix the UI lockup. Fixes Issue #132

--- source/javascript/Container.js
+++ source/javascript/Container.js
@@ -228,13 +228,13 @@ enyo.kind({
 
 	refreshAll: function(account_id) {
 		this.loadingColumns = 0;
-		
+
 		var opts = { };
 		if(account_id) {
 			opts.account_id = account_id;
 			opts.column_types = [SPAZ_COLUMN_HOME, SPAZ_COLUMN_SENT];
 		}
-		
+
 		if(this.columnsFunction("loadNewer", opts) === 0) {
 			this.loadFinished();
 		}
@@ -319,7 +319,8 @@ enyo.kind({
 			var column = this.columnData.splice(del_idx, 1)[0];
 			this.columnData.splice(new_idx, 0, column);
 
-			this.createColumns();
+			setTimeout(function() { this.createColumns(); }, 1);
+
 			//@TODO: this creates issues with starting a column drag. To replicate: Hold onto a column toolbar, move it slightly, release. It will float in an awkward postion.
 			//this doesn't happen if this.createColumns() is commented out.
 		}
