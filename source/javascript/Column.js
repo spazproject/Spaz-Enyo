fbbc93f88ae056b5e6f38a9aa5b1a30a681ce910
Ed Finkler <coj@funkatron.com>
2011-08-04 00:03:51 -0400

    merge n fix

--- source/javascript/Column.js
+++ source/javascript/Column.js
@@ -48,8 +48,8 @@ enyo.kind({
 				{name: "topRightButton", kind: "ToolButton", icon: "source/images/icon-close.png", onclick: "deleteClicked"}
 		]},
 		{kind: enyo.VFlexBox, flex: 1, className: "timeline", components: [
-				{name: "pulltoRefreshTextTeaser", className: "ptrTeaser", content: "", showing: false},
-				{name: "list", kind: "Spaz.VirtualList", flex: 1, horizontal: false, className: "list", onAcquirePage:'acquirePage', onSetupRow: "setupRow", components: [
+				{name: "pulltoRefreshTextTeaser", className: "ptrTeaser", content: "Release to refresh &uarr;", showing: false},
+				{name: "list", kind: "Spaz.VirtualList", flex: 1, horizontal: false, className: "list", onAcquirePage:'acquirePage', onSetupRow: "setupRow", onPullToRefresh: "pullToRefresh", components: [
 					{
 						name: "item",
 						kind: "Spaz.Entry",
@@ -570,6 +570,8 @@ enyo.kind({
 				this.$.header.setContent(_.capitalize(this.info.type));
 			}
 		}
+
+		this.$.pulltoRefreshTextTeaser.width = this.width;
 	},
 	refreshList: function(forceReload){
 		this.$.list.refresh();
--- vendors/ekl/List/VirtualList.js
+++ vendors/ekl/List/VirtualList.js
@@ -1,99 +1,100 @@
 enyo.kind({
-    name: "ekl.List.VirtualList",
-    kind: enyo.VirtualList,
-    
-    dragHoldTime: 0,
-    
-    published: {
-        mousewheel: true,
-        //Dampens mousewheel delta strength
-        mousewheelDamp: 40.0,
-        // sensitivity for pull-to-refresh events
+	name: "ekl.List.VirtualList",
+	kind: enyo.VirtualList,
+
+	dragHoldTime: 0,
+
+	published: {
+		mousewheel: true,
+		//Dampens mousewheel delta strength
+		mousewheelDamp: 40.0,
+		// sensitivity for pull-to-refresh events
 		dragHoldInterval: 100,
-	    dragHoldTrigger: 300,
-    },
-    events: {
-		onPullToRefresh: ""
-    },
-
-    mousewheelHandler: function(inSender, inEvent) {
-        if (this.mousewheel) {
-                //if at top, don't let you scroll up.
-            //if(this.$.scroller.pageTop < 0 || inEvent.delta.y < 0){
-
-                //Clone event
-                var dragTo = enyo.mixin({}, inEvent);
-                //Apply delta to new event
-                dragTo.pageX = inEvent.pageX + (inEvent.delta.x * this.mousewheelDamp);
-                dragTo.pageY = inEvent.pageY + (inEvent.delta.y * this.mousewheelDamp);
-
-                //Simulate initiating a drag
-                this.$.scroller.$.scroll.startDrag(inEvent);
-                //Simulate dragging to a point
-                this.$.scroller.$.scroll.drag(dragTo);
-                //Simulate dropping a drag at the same point (prevents flick, lets OS provide accelleration)
-                this.$.scroller.$.scroll.dragDrop(dragTo);
-                //Simulate ending a drag
-                this.$.scroller.$.scroll.dragFinish();
-           //}
-
-        }
-    },
-	holdMousePoller: function() {
-			console.log(this.dragHoldTime);
-			if(this.dragHoldTime > this.dragHoldInterval*4) {
-				this.owner.$.pulltoRefreshTextTeaser.setShowing(true);
-			}
-			this.owner.$.pulltoRefreshTextTeaser.content = 'Release to Refresh ';//+(this.dragHoldTrigger-this.dragHoldTime).toString();
-			this.owner.$.pulltoRefreshTextTeaser.render();
-			if(this.$.scroller.$.scroll.isInOverScroll()) {
-				this.dragHoldTime+=this.dragHoldInterval;
-			} else {
-				this.dragHoldInterval = 0;
-			}
-				
-			dragPoller = window.setTimeout(this.holdMousePoller.bind(this), this.dragHoldInterval);
+		dragHoldTrigger: 300,
+		dragHoldTimeMax: 5000, // if a pull exceeds this time without firing, exite
+		pullToRefreshThreshold: 50, // px
 	},
-    mousedownHandler: function() {
-		dragPoller = window.setTimeout(this.holdMousePoller.bind(this), this.dragHoldInterval);
-	/*
-	console.log('mousedown');
-		if(this.dragHoldTime == 0) {
-			dragPoller = window.setTimeout(function() {
-				console.log('dragpoller running');
-				if(this.$.scroller.$.scroll.isInOverScroll()) {
-					console.log(this.dragHoldInterval);
-					if(this.dragHoldTime > this.dragHoldInterval*4) {
-						this.owner.$.pulltoRefreshTextTeaser.setShowing(true);
-					}
-					this.owner.$.pulltoRefreshTextTeaser.content = 'Release to Refresh ';//+(this.dragHoldTrigger-this.dragHoldTime).toString();
-					this.owner.$.pulltoRefreshTextTeaser.render();
-					this.dragHoldTime+=this.dragHoldInterval;
-					dragPoller = window.setTimeout(this,this.dragHoldInterval);
-				} else {
-					console.log('not in overscroll');
-				}
-			}.bind(this),100);
-		} else {
-			console.log('dragholdtime not zero');
+	events: {
+		onPullToRefresh: ""
+	},
+	dragPoller: null,
+
+	mousewheelHandler: function(inSender, inEvent) {
+		if (this.mousewheel) {
+			//Clone event
+			var dragTo = enyo.mixin({}, inEvent);
+			//Apply delta to new event
+			dragTo.pageX = inEvent.pageX + (inEvent.delta.x * this.mousewheelDamp);
+			dragTo.pageY = inEvent.pageY + (inEvent.delta.y * this.mousewheelDamp);
+
+			//Simulate initiating a drag
+			this.$.scroller.$.scroll.startDrag(inEvent);
+			//Simulate dragging to a point
+			this.$.scroller.$.scroll.drag(dragTo);
+			//Simulate dropping a drag at the same point (prevents flick, lets OS provide accelleration)
+			this.$.scroller.$.scroll.dragDrop(dragTo);
+			//Simulate ending a drag
+			this.$.scroller.$.scroll.dragFinish();
 		}
-	*/
-    },
-    mouseupHandler: function(e) {
-		console.log('mouseup');
-		window.clearTimeout(dragPoller);
-		if(this.dragHoldTime >= this.dragHoldTrigger && this.$.scroller.$.scroll.isInOverScroll()) {
-		
+	},
+	holdMousePoller: function() {
+		console.log('mousepoll', this.$.scroller.$.scroll.y);
+		if (this.dragHoldTimeMax < this.dragHoldTime) {
+			window.clearTimeout(this.dragPoller);
+			console.log("hit max; exiting");
+			return;
+		}
+
+		if ((this.dragHoldTime > this.dragHoldInterval*4) && this.pulledPastThreshold()) {
+			this.owner.$.pulltoRefreshTextTeaser.setShowing(true);
+		}
+		this.owner.$.pulltoRefreshTextTeaser.render();
+		if (this.$.scroller.$.scroll.y >= 0) {
+			// console.log('continuing poller', this.$.scroller.$.scroll.y);
+			this.dragHoldTime+=this.dragHoldInterval;
+		} else {
+			// console.log('cancelling poller', this.$.scroller.$.scroll.y);
+
+			// always clear before starting timeout
+			window.clearTimeout(this.dragPoller);
+			this.dragHoldInterval = 0;
+			return;
+		}
+
+		// always clear before starting timeout
+		window.clearTimeout(this.dragPoller);
+		this.dragPoller = window.setTimeout(_.bind(this.holdMousePoller, this), this.dragHoldInterval);
+	},
+	mousedownHandler: function() {
+
+		if (this.$.scroller.$.scroll.y < 0) {
+			return;
+		}
+		// always clear before starting timeout
+		window.clearTimeout(this.dragPoller);
+		this.dragPoller = window.setTimeout(_.bind(this.holdMousePoller, this), this.dragHoldInterval);
+
+		// console.log('mousedown', this.$.scroller.$.scroll.y);
+
+	},
+	mouseupHandler: function(e) {
+		// console.log('mouseup', this.$.scroller.$.scroll.y);
+
+		window.clearTimeout(this.dragPoller);
+		if(this.dragHoldTime >= this.dragHoldTrigger && this.pulledPastThreshold()) {
+			console.log('firing pull to refresh');
+			this.doPullToRefresh();
 		}
 		this.dragHoldTime = 0;
-	/*
-		this.owner.$.pulltoRefreshTextTeaser.setShowing(false);
-    	window.clearTimeout(dragPoller);
-    	if(this.$.scroller.$.scroll.isInOverScroll() && this.dragHoldTime > this.dragHoldTrigger) {
-			//this.doPullToRefresh();	
-    	}
-    	this.dragHoldTime = 0;
-	*/
-    }
 
+		this.owner.$.pulltoRefreshTextTeaser.setShowing(false);
+	},
+
+
+	pulledPastThreshold: function(e) {
+		if (this.$.scroller.$.scroll.y >= this.pullToRefreshThreshold) {
+			return true;
+		}
+		return false;
+	}
 });
