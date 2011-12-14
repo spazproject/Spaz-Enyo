f1355515a1ba314a5c3e1263a45bd8ccdffac568
Nicholas Guarracino <nick@guarracino.org>
2011-12-13 21:37:02 -0500

    New spazcore build to support lockerz.

--- vendors/spazcore.js
+++ vendors/spazcore.js
@@ -1,4 +1,4 @@
-/*********** Built 2011-12-13 10:29:21 EST ***********/
+/*********** Built 2011-12-13 21:31:38 EST ***********/
 /*jslint
 browser: true,
 nomen: false,
@@ -9867,16 +9867,14 @@ SpazImageURL.prototype.initAPIs = function() {
 	});
 
 
-	this.addAPI('tweetphoto', {
-		'url_regex'       : /http:\/\/tweetphoto.com\/([a-zA-Z0-9]+)/gi,
+	this.addAPI('lockerz', {
+		'url_regex'       : /http:\/\/lockerz.com\/s\/([a-zA-Z0-9]+)/gi,
 		'getThumbnailUrl' : function(id) {
-			// http://TweetPhotoAPI.com/api/TPAPI.svc/json/imagefromurl?size=thumbnail&url=http://tweetphoto.com/iyb9azy4
-			var url = 'http://TweetPhotoAPI.com/api/TPAPI.svc/imagefromurl?size=thumbnail&url=http://tweetphoto.com/'+id;
+			var url = 'http://api.plixi.com/api/tpapi.svc/imagefromurl?url=http://plixi.com/p/'+id+'&size=small';
 			return url;
 		},
 		'getImageUrl'     : function(id) {
-			// http://TweetPhotoAPI.com/api/TPAPI.svc/imagefromurl?size=big&url=http://tweetphoto.com/iyb9azy4
-			var url = 'http://TweetPhotoAPI.com/api/TPAPI.svc/imagefromurl?size=big&url=http://tweetphoto.com/'+id;
+			var url = 'http://api.plixi.com/api/tpapi.svc/imagefromurl?url=http://plixi.com/p/'+id+'&size=big';
 			return url;
 		}
 	});
